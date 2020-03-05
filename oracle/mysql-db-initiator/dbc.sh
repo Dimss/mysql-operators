3645d4eff389ab8a2d876c964d95f520c5287323
Dmitry Kartsev <dkartsev@redhat.com>
2020-03-05 11:32:59 +0200

    updates

--- oracle/mysql-db-initiator/dbc.sh
+++ oracle/mysql-db-initiator/dbc.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-set -x
-MYSQL_HOST=${MYSQL_CR_NAME}
+set -xe
+MYSQL_HOST=${CLUSTER_NAME}
 # By default Oracle operator will create MySQL root password
 MYSQL_USER=root
 # Get MySQL root password from K8S secret ${MYSQL_CR_NAME}-root-password
-MYSQL_PASS=$(oc get secret ${MYSQL_CR_NAME}-root-password -ojsonpath="{.data.password}" | base64 -d)
+MYSQL_PASS=$(oc get secret ${CLUSTER_NAME}-root-password -ojsonpath="{.data.password}" | base64 -d)
 # Create a new DB
-mysql -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASS} -e "CREATE DATABASE ${MYSQL_CR_NAME};"
+mysql -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASS} -e "CREATE DATABASE ${CLUSTER_NAME};"
 # List all DBs
 mysql -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASS} -e "SHOW DATABASES;"
